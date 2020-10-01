13aacaf4938d974f152c5a424197a37a67af6a82
Shanatnu Raut <67189790+s4shantanu@users.noreply.github.com>
2020-10-01 16:05:37 +0530

    Add files via upload

---
+++ favLanguage.py
@@ -0,0 +1,11 @@
+favLang = {}
+a = input("Enter your favorite language Shantanu\n")
+b = input("Enter your favorite language Ankit\n")
+c = input("Enter your favorite language Rajat")
+d = input("Enter your favorite language Harshita\n")
+favLang['shubham'] = a
+favLang['ankit'] = b
+favLang['sonali'] = c
+favLang['harshita'] = d
+
+print(favLang)
